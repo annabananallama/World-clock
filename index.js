6c0c3cb83353be68ba18195a17afb708de170714
annabananallama <annasimonelarsen@gmail.com>
2023-05-16 02:13:45 +0100

    update for selected element
    selected country shows with date and time

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
-function updateDateTime() {
-  let londonElement = document.querySelector(".london");
-  if (londonElement) {
-    let londonDateElement = londonElement.querySelector(".date");
-    let londonTimeElement = londonElement.querySelector(".time");
+let timerInterval;
 
-    setInterval(function () {
-      let londonTime = moment().tz("Europe/London");
-      londonDateElement.innerHTML = londonTime.format("DD MMMM Y");
-      londonTimeElement.innerHTML = londonTime.format("h:mm:ss A");
-    }, 1000);
+function startTimer(cityTime, dateElements, timeElements) {
+  clearInterval(timerInterval);
+
+  for (let i = 0; i < dateElements.length; i++) {
+    dateElements[i].innerHTML = cityTime.format("DD MMMM Y");
   }
 
-  let tokyoElement = document.querySelector(".tokyo");
-  if (tokyoElement) {
-    let tokyoDateElement = tokyoElement.querySelector(".date");
-    let tokyoTimeElement = tokyoElement.querySelector(".time");
+  function updateTime() {
+    for (let i = 0; i < timeElements.length; i++) {
+      timeElements[i].innerHTML = cityTime.format("h:mm:ss A");
+    }
+  }
 
-    setInterval(function () {
-      let tokyoTime = moment().tz("Asia/Tokyo");
-      tokyoDateElement.innerHTML = tokyoTime.format("DD MMMM Y");
-      tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss A");
-    }, 1000);
+  updateTime();
+
+  timerInterval = setInterval(updateTime, 1000);
+}
+
+function updateCity(event) {
+  let newCityElement = document.getElementById("londonName");
+  let newDateElements = document.querySelectorAll(".update .date");
+  let newTimeElements = document.querySelectorAll(".update .time");
+  let cairoTime = moment().tz("Africa/Cairo");
+  let sanFranciscoTime = moment().tz("America/Los_Angeles");
+  let londonTime = moment().tz("Europe/London");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  clearInterval(timerInterval);
+
+  if (event && event.target.value === "cairo") {
+    newCityElement.innerHTML = "Cairo";
+    startTimer(cairoTime, newDateElements, newTimeElements);
+  } else if (event && event.target.value === "sanFransisco") {
+    newCityElement.innerHTML = "San Francisco";
+    startTimer(sanFranciscoTime, newDateElements, newTimeElements);
+  } else if (event && event.target.value === "tokyo") {
+    newCityElement.innerHTML = "Tokyo";
+    startTimer(tokyoTime, newDateElements, newTimeElements);
+  } else {
+    newCityElement.innerHTML = "London";
+    startTimer(londonTime, newDateElements, newTimeElements);
   }
 }
 
-updateDateTime();
+function initializeCity() {
+  let selectElement = document.getElementById("city-selector");
+  let selectedCity = selectElement.value;
+
+  updateCity({ target: { value: selectedCity } });
+}
+
+const selectElement = document.getElementById("city-selector");
+selectElement.addEventListener("change", updateCity);
+
+initializeCity();
